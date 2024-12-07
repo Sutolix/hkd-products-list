733753e05811498a67d139d899dec8d711ee5a82
Sutolix <tg884ght@gmail.com>
2024-12-07 13:47:37 -0300

    update selectors

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,33 +1,33 @@
 jQuery(document).ready(function ($) {
     // Abas
-    $(".hpl_tab-item").on("click", function () {
+    $(".hpl-tab-item").on("click", function () {
         const tabId = $(this).data("tab");
 
-        $(".hpl_tab-item").removeClass("hpl_active");
-        $(".hpl_tab-content").removeClass("hpl_active");
+        $(".hpl-tab-item").removeClass("hpl-active");
+        $(".hpl-tab-content").removeClass("hpl-active");
 
-        $(this).addClass("hpl_active");
-        $("#" + tabId).addClass("hpl_active");
+        $(this).addClass("hpl-active");
+        $("#" + tabId).addClass("hpl-active");
     });
 
     // Modal
-    $(".hpl_buy-button").on("click", function () {
+    $(".hpl-buy-button").on("click", function () {
         const productName = $(this).data("product-name");
         const productPrice = $(this).data("product-price");
         const productImage = $(this).data("product-image");
         const variationName = $(this).data("variation-name");
         const addToCartUrl = $(this).data("add-to-cart");
 
-        $(".hpl_modal-product-thumbnail").attr("src", productImage);
-        $(".hpl_modal-product-name").text(productName);
-        $(".hpl_modal-variation-name").text(variationName);
-        $(".hpl_modal-product-price").html(productPrice);
-        $(".hpl_modal-confirm").attr("href", addToCartUrl);
+        $(".hpl-modal-product-thumbnail").attr("src", productImage);
+        $(".hpl-modal-product-name").text(productName);
+        $(".hpl-modal-variation-name").text(variationName);
+        $(".hpl-modal-product-price").html(productPrice);
+        $(".hpl-modal-confirm").attr("href", addToCartUrl);
 
-        $(".hpl_modal").fadeIn();
+        $(".hpl-modal").fadeIn();
     });
 
-    $(".hpl_modal-close").on("click", function () {
-        $(".hpl_modal").fadeOut();
+    $(".hpl-modal-close").on("click", function () {
+        $(".hpl-modal").fadeOut();
     });
 });
