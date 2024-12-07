7cf8676cd7f6b1a45572d39b95c4cf40386427d8
Sutolix <tg884ght@gmail.com>
2024-12-07 11:08:57 -0300

    List variable products instead categories and create base functionalities to tabs and modal

--- widgets/products-list.php
+++ widgets/products-list.php
@@ -37,19 +37,29 @@ class Elementor_HPL_Products_List extends \Elementor\Widget_Base
         return ['hpl-products-list-script'];
     }
 
-    protected function register_controls(): void
+    private function get_variable_products()
     {
-        $product_categories = get_terms([
-            'taxonomy'   => 'product_cat',
-            'hide_empty' => false,
-        ]);
+        $args = [
+            'status'    => 'publish',
+            'orderby' => 'name',
+            'order'   => 'ASC',
+            'limit' => -1,
+        ];
+        $all_products = wc_get_products($args);
 
-        $categories_array = [];
-        if (!is_wp_error($product_categories)) {
-            foreach ($product_categories as $category) {
-                $categories_array[$category->slug] = $category->name;
+        $products_array = [];
+        foreach ($all_products as $product) {
+            if ($product->get_type() == "variable") {
+                $products_array[$product->get_id()] = $product->get_name();
             }
         }
+
+        return $products_array;
+    }
+
+    protected function register_controls(): void
+    {
+        $products = $this->get_variable_products();
 
         $this->start_controls_section(
             'content_section',
@@ -60,24 +70,34 @@ class Elementor_HPL_Products_List extends \Elementor\Widget_Base
         );
 
         $this->add_control(
-            'categories',
+            'products',
             [
-                'label' => 'Categorias',
+                'label' => 'Produtos',
                 'type' => \Elementor\Controls_Manager::REPEATER,
                 'fields' => [
                     [
-                        'name' => 'woo_category',
-                        'label' => 'Aba da categoria',
+                        'name' => 'product_id',
+                        'label' => 'Aba do produto',
                         'type' => \Elementor\Controls_Manager::SELECT,
                         'label_block' => true,
-                        'default' => 'Item',
-                        'options' => $categories_array,
+                        'default' => 'Produto',
+                        'options' => $products,
                         'selectors' => [
                             '{{WRAPPER}} .your-class' => 'border-style: {{VALUE}};',
                         ],
                     ],
                 ],
-                'title_field' => '{{{ woo_category }}}',
+                'title_field' => 'Produto #{{{ product_id }}}',
+            ]
+        );
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'style_section',
+            [
+                'label' => esc_html__('Style', 'elementor'),
+                'tab' => \Elementor\Controls_Manager::TAB_STYLE,
             ]
         );
 
@@ -88,6 +108,74 @@ class Elementor_HPL_Products_List extends \Elementor\Widget_Base
     {
         $settings = $this->get_settings_for_display();
 
-        var_dump($settings['categories']);
+        if (empty($settings['products'])) return;
+
+        $tabHeaders = '';
+        $tab_contents = '';
+
+        foreach ($settings['products'] as $product) {
+            $product = wc_get_product($product['product_id']);
+
+            if (!$product || !$product->is_type('variable')) {
+                continue;
+            }
+
+            $product_name = $product->get_name();
+            $product_slug = $product->get_slug();
+            $product_image = wp_get_attachment_url($product->get_image_id());
+
+            // Criação da aba
+            $tabHeaders .= "<li class='hpl_tab-item' data-tab='hpl_tab-{$product_slug}'>";
+            if ($product_image) {
+                $tabHeaders .= "<img src='{$product_image}' alt='{$product_name}' class='hpl_product-thumbnail' />";
+            }
+            $tabHeaders .= "<span>{$product_name}</span>";
+            $tabHeaders .= "</li>";
+
+            // Variações do produto
+            $tab_contents .= "<div class='hpl_tab-content' id='hpl_tab-{$product_slug}'>";
+
+            $variation_ids = $product->get_children();
+            foreach ($variation_ids as $variation_id) {
+                $variation = wc_get_product($variation_id);
+
+                $attributes = $variation->get_variation_attributes();
+                $variation_name = is_array($attributes) ? array_pop(array_reverse($attributes)) : $product_name;
+                $variation_price = $variation->get_price_html();
+                $add_to_cart_url = wc_get_cart_url() . "?add-to-cart={$product->get_id()}&variation_id={$variation->get_id()}";
+
+                $tab_contents .= "<div class='hpl_variation-item'>";
+                $tab_contents .= "<h4>{$variation_name}</h4>";
+                $tab_contents .= "<button class='hpl_buy-button' 
+                                        data-product-name='{$product_name}' 
+                                        data-product-price='{$variation_price}' 
+                                        data-product-image='{$product_image}' 
+                                        data-variation-name='{$variation_name}' 
+                                        data-add-to-cart='{$add_to_cart_url}'>
+                                        Comprar
+                                     </button>";
+                $tab_contents .= "</div>";
+            }
+            $tab_contents .= "</div>";
+        }
+
+        echo "<div class='elementor-widget-hpl_products_list'>";
+        echo "<ul class='hpl_tabs-header'>{$tabHeaders}</ul>";
+        echo "<div class='hpl_tabs-content'>{$tab_contents}</div>";
+        echo "</div>";
+
+        // Modal
+        echo "<div class='hpl_modal' style='display:none;'>
+                    <div class='hpl_modal-content'>
+                        <h2>Confirme a sua compra</h2>
+                        <p>Você está comprando COINS para a plataforma</p>
+                        <img class='hpl_modal-product-thumbnail' src='' alt=''>
+                        <h3 class='hpl_modal-product-name'></h3>
+                        <p>Quantidade: <span class='hpl_modal-variation-name'></span> | Valor total: <span class='hpl_modal-product-price'></span></p>
+                        <p>Possui cupom? Você vai poder usar na etapa de pagamento.</p>
+                        <button class='hpl_modal-close'>Voltar</button>
+                        <a href='#' class='hpl_modal-confirm'>Confirmar</a>
+                    </div>
+                 </div>";
     }
 }
