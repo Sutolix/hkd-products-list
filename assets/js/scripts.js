eae341542813a895fea3ca44efc166520fcb38d9
Sutolix <tg884ght@gmail.com>
2024-12-08 09:23:46 -0300

    change trigger mode to work on preview too

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,33 +1,40 @@
-jQuery(document).ready(function ($) {
-    // Abas
-    $(".hpl-tab-item").on("click", function () {
-        const tabId = $(this).data("tab");
+(function($){
+    $(window).on('elementor/frontend/init', function () {
+        function initializeEvents() {
+            // Abas
+            $(".hpl-tab-item").on("click", function () {
+                const tabId = $(this).data("tab");
+        
+                $(".hpl-tab-item").removeClass("hpl-active");
+                $(".hpl-tab-content").removeClass("hpl-active");
+        
+                $(this).addClass("hpl-active");
+                $("#" + tabId).addClass("hpl-active");
+            });
+        
+            // Modal
+            $(".hpl-buy-button").on("click", function () {
+                const productName = $(this).data("product-name");
+                const productPrice = $(this).data("product-price");
+                const productImage = $(this).data("product-image");
+                const variationName = $(this).data("variation-name");
+                const addToCartUrl = $(this).data("add-to-cart");
+        
+                $(".hpl-modal-product-thumbnail").attr("src", productImage);
+                $(".hpl-modal-product-name").text(productName);
+                $(".hpl-modal-variation-name").text(variationName);
+                $(".hpl-modal-product-price").html(productPrice);
+                $(".hpl-modal-confirm").attr("href", addToCartUrl);
+        
+                $(".hpl-modal").fadeIn();
+            });
+        
+            $(".hpl-modal-close").on("click", function () {
+                $(".hpl-modal").fadeOut();
+            });
+        }
 
-        $(".hpl-tab-item").removeClass("hpl-active");
-        $(".hpl-tab-content").removeClass("hpl-active");
-
-        $(this).addClass("hpl-active");
-        $("#" + tabId).addClass("hpl-active");
+        elementorFrontend.hooks.addAction('frontend/element_ready/hpl_products_list.default', initializeEvents);
     });
+}(jQuery));
 
-    // Modal
-    $(".hpl-buy-button").on("click", function () {
-        const productName = $(this).data("product-name");
-        const productPrice = $(this).data("product-price");
-        const productImage = $(this).data("product-image");
-        const variationName = $(this).data("variation-name");
-        const addToCartUrl = $(this).data("add-to-cart");
-
-        $(".hpl-modal-product-thumbnail").attr("src", productImage);
-        $(".hpl-modal-product-name").text(productName);
-        $(".hpl-modal-variation-name").text(variationName);
-        $(".hpl-modal-product-price").html(productPrice);
-        $(".hpl-modal-confirm").attr("href", addToCartUrl);
-
-        $(".hpl-modal").fadeIn();
-    });
-
-    $(".hpl-modal-close").on("click", function () {
-        $(".hpl-modal").fadeOut();
-    });
-});
